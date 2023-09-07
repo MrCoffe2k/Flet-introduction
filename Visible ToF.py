e46a105fddbf5b0aadb9a0a38a49e535d5ed1c14
Jordi Mauricio <114832904+MrCoffe2k@users.noreply.github.com>
2023-09-07 10:53:25 -0600

    Comentarios

--- Visible ToF.py
+++ Visible ToF.py
@@ -1,13 +1,18 @@
 import flet as ft
 
 def main(page: ft.page):
+    # Define el color de la ventana
     page.window_bgcolor = ft.colors.BLUE_GREY
     page.bgcolor = ft.colors.BLUE_GREY
+    # Campos de texto para nombre y apellido
     first_name = ft.TextField()
     last_name = ft.TextField()
+    # Define si el widget será interactuable
     first_name.disabled = False
     last_name.disabled = False
+    # Actualiza los widgets
     page.update()
+    # Renderiza los widgets correspondientes
     page.add(first_name, last_name)
 
 ft.app(target=main)
